4b91604bd4a99055200da4afd9e35f5ba17ef663
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2026-06-11 10:23:35 +0200

    mounting frontend in the FastAPI instance

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from starlette.staticfiles import StaticFiles
 
 from backend.core.settings import ensure_dirs
 from backend.api import datasets, configs, evaluation, postprocessing, results #rights,
@@ -22,14 +24,26 @@ def create_app() -> FastAPI:
         print("INCOMING:", request.method, request.url.path)
         return await call_next(request)
 
-    app.include_router(datasets.router)
-    app.include_router(configs.configs_router)
-    app.include_router(configs.rights_router)
-    app.include_router(postprocessing.router)
+
+    app_api = FastAPI()
+    app_api.include_router(datasets.router)
+    app_api.include_router(configs.configs_router)
+    app_api.include_router(configs.rights_router)
+    app_api.include_router(postprocessing.router)
     #app.include_router(rights.router)
-    app.include_router(evaluation.router)
-    app.include_router(results.router)
-    
+    app_api.include_router(evaluation.router)
+    app_api.include_router(results.router)
+
+
+    app.mount('/api', app_api)
+
+    # Calculate absolute path to frontend/dist directory
+    _script_dir = os.path.dirname(os.path.abspath(__file__))
+    _project_root = os.path.dirname(_script_dir)
+    _frontend_dist = os.path.join(_project_root, 'frontend', 'dist')
+    app.mount('/', StaticFiles(directory=_frontend_dist, html=True), name='static')
+
+
     return app
 
 app = create_app()
